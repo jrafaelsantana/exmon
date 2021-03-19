91619072ca0c028e58dab3312d7dba3a799f4d60
Rafael Santana <jrafaeldesantana@gmail.com>
2021-03-18 21:51:23 -0300

    Attack and heal

--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -1,6 +1,7 @@
 defmodule ExMon.Game.Actions do
   alias ExMon.Game
   alias ExMon.Game.Actions.Attack
+  alias ExMon.Game.Actions.Heal
 
   def fetch_move(move) do
     Game.player()
@@ -16,8 +17,15 @@ defmodule ExMon.Game.Actions do
 
   def attack(move) do
     case Game.turn() do
-      :player -> Attack.attack_opponent(:computador, move)
+      :player -> Attack.attack_opponent(:computer, move)
       :computer -> Attack.attack_opponent(:player, move)
+    end
+  end
+
+  def heal() do
+    case Game.turn() do
+      :player -> Heal.heal_life(:player)
+      :computer -> Heal.heal_life(:computer)
     end
   end
 end
